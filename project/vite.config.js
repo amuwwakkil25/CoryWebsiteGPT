132d1278581c8650ad9c60a5a9008008720b4674
amuwwakkil25 <40582737+amuwwakkil25@users.noreply.github.com>
2025-08-10 21:49:55 -0500

    Add files via upload

---
+++ project/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        'how-it-works': resolve(__dirname, 'how-it-works.html'),
+        features: resolve(__dirname, 'features.html'),
+        'roi-and-case-studies': resolve(__dirname, 'roi-and-case-studies.html'),
+        'demo-and-pricing': resolve(__dirname, 'demo-and-pricing.html'),
+        resources: resolve(__dirname, 'resources.html'),
+        privacy: resolve(__dirname, 'privacy.html')
+      }
+    }
+  }
+})
