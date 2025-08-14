09c32857252d5bbad45597aab5c67576d886bbce
amuwwakkil25 <40582737+amuwwakkil25@users.noreply.github.com>
2025-08-14 13:29:54 -0500

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
         'roi-and-case-studies': resolve(__dirname, 'roi-and-case-studies.html'),
         'demo-and-pricing': resolve(__dirname, 'demo-and-pricing.html'),
         resources: resolve(__dirname, 'resources.html'),
-        privacy: resolve(__dirname, 'privacy.html')
+        privacy: resolve(__dirname, 'privacy.html'),
+        'voice-demo': resolve(__dirname, 'voice-demo.html')
       }
     }
   }
