2ea1706dc92625b52ff3632fc427ddbeca48e748
amuwwakkil25 <40582737+amuwwakkil25@users.noreply.github.com>
2025-09-04 12:06:12 -0500

    Updated resources.ts

--- assets/js/resources.ts
+++ assets/js/resources.ts
@@ -330,9 +330,13 @@ class DatabaseService {
       DiagnosticLogger.log('Testing connection to:', { testUrl });
       
       const response = await fetch(testUrl, {
+      }
+      )
       DiagnosticLogger.log('Testing connection to:', { testUrl });
       
       const response = await fetch(testUrl, {
+      }
+      )
       DiagnosticLogger.log('Testing connection to:', { testUrl });
       
       const response = await fetch(testUrl, {
